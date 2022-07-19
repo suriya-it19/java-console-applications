e469e0afb658b4eb61c37b5c7f892b3dbceb6057
Suriya Senthilkumar <54205777+suriya-it19@users.noreply.github.com>
2022-07-19 16:40:06 +0530

    Create Entry.java

---
+++ BankApplication/Entry.java
@@ -0,0 +1,16 @@
+package BankApplication;
+
+public class Entry {
+    public String choice() {}
+    // admin or customer
+
+    // get customer choice - withdraw, drop, check balance
+    public String customerChoice() {}
+    
+    // get admin choice - check total amount, check specific customer details
+    public void adminChoice() {}
+
+
+    // main layout to get inputs
+
+}
